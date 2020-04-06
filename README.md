45a261b2dda682b0bc0dac3c079231c34b0f8dcf
vcka <vcka@ministras.lt>
2020-04-06 20:32:23 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# 🍿 How to stream Torrent movies using NodeJS and the HTML5 video tag
+
+After working on the [Understanding Streams in NodeJS](https://github.com/davidgatti/Understanding-Streams-in-NodeJS) article, I moved on to [How to Stream Movies using NodeJS](https://github.com/davidgatti/How-to-Stream-Movies-using-NodeJS). Then I thought... *Can I stream a Torrent in real time*? Turns out that yes, I can! And it's surprisingly easy, thanks to the [WebTorrent](https://webtorrent.io) npm module.
+
+And, to be honest, I'm completely blown away by this technology. I mean, the potential here is endless. I still can't believe that I didn't get it sooner. But better now than never.  ;)
+
+# What is this repository
+
+This repo is a tech demo/proof of concept/article showcasing what can you do using NodeJS and the WebTorrent module. This is also a good example of how powerful the idea of streams is in general. Because you'll see that even before the file is completely downloaded to the disk, we can read the chunks of data that we have. Proving that we don't need the whole thing to stream data to the client.
+
+# Deployment
+
+<a href="https://heroku.com/deploy?template=https://github.com/davidgatti/How-to-Stream-Torrents-using-NodeJS" target="_blank">
+<img align="left" style="float: left; margin: 0 10px 0 0;" src="https://www.herokucdn.com/deploy/button.svg"></a>
+
+Follow the instructions on the Heroku main app page, and then once the app is deployed, visit the main page. Once you do that, you should see the site, with some examples of free Magnet Hashes so you can test out the live streaming.
+
+# Where to start
+
+1. Visit the home page of your deployment
+1. Click on one of the demo Hashes
+1. You should see the Client Stats start to show some stats
+1. In a moment, you should see the content of the Magnet Hash
+1. Select a movie file from the Magnet Contents section
+1. After few seconds, you should see the video player buffering the movie
+1. It should start playing shortly
+
+And there you have it! Proof that you are streaming a Torrent live.
+
+# High level explanation
+
+We know how we can stream binary files, thanks to the `.createReadStream()` method, which gives us the possibility of specifying how much data should be read by setting the starting and finishing position of the chunk that we are interested in.
+
+With `.createReadStream()`, we don't care if the whole file is actually on the local drive, until we have the right section, we can read it and send it, while in the background the file is being constantly downloaded.
+
+This also proves that we're actually dealing with only chunks, and are not loading the whole file into memory. If you want to learn more about streams in NodeJS, check out my previous article titled [How to Stream Movies using NodeJS](https://github.com/davidgatti/How-to-Stream-Movies-using-NodeJS).
+
+
+# How to understand the code
+
+The code is composed of two parts: We have the front-end and we have the back-end. Shocking, I know. The good part is that the front has no UI design and only two lines of CSS. You can focus on the code itself. Aside from having very detailed comments, the code is also numbered to help you follow what is happening within each step.
+
+The back-end is similar. If you read the [How to Stream Movies using NodeJS](https://github.com/davidgatti/How-to-Stream-Movies-using-NodeJS) article first, you see only new code related to the [WebTorrent](https://webtorrent.io) module. Meaning that if you feel confused, you should check [How to Stream Movies using NodeJS](https://github.com/davidgatti/How-to-Stream-Movies-using-NodeJS) first to understand the basics of streaming videos with NodeJS.
+
+# The End
+
+If you've enjoyed this article/project, please consider giving it a 🌟 or donate.
+
+- [![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.me/gattidavid/25)
+- [![Star on GitHub](https://img.shields.io/github/stars/davidgatti/How-to-Stream-Torrents-using-NodeJS.svg?style=social)](https://github.com/davidgatti/How-to-Stream-Movies-using-NodeJS/stargazers)
+- [![Watch on GitHub](https://img.shields.io/github/watchers/davidgatti/How-to-Stream-Torrents-using-NodeJS.svg?style=social)](https://github.com/davidgatti/How-to-Stream-Movies-using-NodeJS/watchers)
+
+Also check out my [GitHub account](https://github.com/davidgatti), where I have other articles and apps that you might find interesting.
+
+## For Hire 👨‍💻
+
+If you'd like me to help you, I'm available for hire. Contact me at job@gatti.pl.
+
+## Where to follow
+
+You can follow me on social media 🐙😇, at the following locations:
+
+- [GitHub](https://github.com/davidgatti)
+- [Twitter](https://twitter.com/dawidgatti)
+- [Instagram](https://www.instagram.com/gattidavid/)
+
+## More about me
+
+I don’t only live on GitHub, I try to do many things not to get bored 🙃. To learn more about me, you can visit the following links:
+
+- [Podcasts](http://david.gatti.pl/podcasts)
+- [Articles](http://david.gatti.pl/articles)
+- [Technical Articles](http://david.gatti.pl/technical_articles)
+- [Software Projects](http://david.gatti.pl/software_projects)
+- [Hardware Projects](http://david.gatti.pl/hardware_projects)
---
+++ routes/video.js
@@ -0,0 +1,443 @@
+let express = require('express');
+let WebTorrent = require('webtorrent')
+let files = [];
+let id = 1;
+let mg = require('magnet-uri');
+const superagent = require('superagent');
+let rimraf = require('rimraf');
+const formidable = require('formidable');
+const fs = require('fs');
+var windows1251 = require('windows-1251');
+let router = express.Router();
+
+//
+//	1.	When the server starts create a WebTorrent client
+//
+let client = new WebTorrent();
+
+//
+//	2.	The object that holds the client stats to be displayed in the front end
+//	using an API call every n amount of time using jQuery.
+//
+let stats = {
+	progress: 0,
+	downloadSpeed: 0,
+	ratio: 0
+}
+
+//
+//	3.	The variable that holds the error message from the client. Farly crude but
+//		I don't expect to much happening hear aside the potential to add the same
+//		Magnet Hash twice.
+//
+let error_message = "";
+
+//
+//	4.	Listen for any potential client error and update the above variable so
+//		the front end can display it in the browser.
+//
+client.on('error', function (err) {
+
+	error_message = err.message;
+
+});
+
+//
+//	5.	Emitted by the client whenever data is downloaded. Useful for reporting the
+//		current torrent status of the client.
+//
+client.on('download', function (bytes) {
+
+	//
+	//	1.	Update the object with fresh data
+	//
+	stats = {
+		progress: Math.round(client.progress * 100 * 100) / 100,
+		downloadSpeed: client.downloadSpeed,
+		ratio: client.ratio
+	}
+
+});
+
+//
+//	API call that adds a new Magnet Hash to the client so it can start
+//	downloading it.
+//
+//	magnet 		-> 	Magnet Hash
+//
+//	return 		<-	An array with a list of files
+//
+
+function getInfo(torrent, magnet, host) {
+	return new Promise((resolve, reject) => {
+		torrent.files.forEach(function (data, index, array) {
+			let movieName;
+			let rating;
+			let length;
+			if (data.name.indexOf("(" | /\d{4}/) !== -1) {
+				movieName = data.name.slice(0, data.name.indexOf("(" | /\d{4}/) - 1).replace(/\./g, ' ')
+			} else if (data.name.indexOf("2O2") !== -1) {
+				movieName = data.name.slice(0, data.name.indexOf("2O")).replace(/\./g, ' ')
+			}
+
+			(async () => {
+				try {
+					if (data.name.endsWith('avi') || data.name.endsWith('mp4') || data.name.endsWith('mkv')) {
+						const res = await superagent.get('www.omdbapi.com/')
+							.query({
+								t: movieName.trim().replace(/\s\s+/g, ' '),
+								plot: 'full',
+								apikey: '47427b38'
+							});
+						files.push({
+							id: id++,
+							title: movieName.trim(),
+							originalTitle: data.name,
+							url: 'https://' + host + '/video/stream/' + magnet + '/' +  encodeURI(data.name),
+							imgUrl: res.body.Poster,
+							length: res.body.Runtime,
+							hash: magnet,
+							rating: res.body.imdbRating,
+							size: data.length,
+							descr: res.body.Plot,
+							country: res.body.Country,
+							actors: res.body.Actors
+						});
+					}
+					if (index === array.length - 1) {
+						setTimeout(resolve, 2000);
+					}
+				} catch (err) {
+					console.error(err);
+				}
+			})();
+		});
+	});
+}
+
+router.get('/add/:magnet', function (req, res) {
+
+	//
+	//	1.	Extract the magnet Hash and save it in a meaningful variable.
+	//
+	//let magnet = req.params.magnet;
+
+	let parsed = mg.decode(req.url.substring(5));
+	let magnet = req.params.magnet;
+
+	if (req.params.magnet === 'magnet:') {
+		magnet = parsed.infoHash;
+		console.log("Got magnet link: " + parsed.dn);
+	} else {
+		magnet = req.params.magnet;
+		console.log("Got infoHash: " + magnet);
+	}
+
+	//
+	//	2.	Add the magnet Hash to the client
+	//
+	client.add(magnet, function (torrent) {
+
+		//
+		//	->	Once we have all the data send it back to the browser to be
+		//		displayed.
+		//
+		console.log(req.get('path'));
+
+		getInfo(torrent, magnet, req.get('host')).then(() => {
+			torrent.pause();
+			res.status(200);
+			res.json({ movies: files });
+			console.log('All done!');
+		});
+	});
+});
+
+//
+//	The API call to start streaming the selected file to the video tag.
+//
+//	magnet 		-> 	Magnet Hash
+//	file_name 	-> 	the selected file name that is within the Magnet Hash
+//
+//	return 		<-	A chunk of the video file as buffer (binary data)
+//
+router.get('/stream/:magnet/:file_name', function (req, res, next) {
+
+	//
+	//	1.	Extract the magnet Hash and save it in a meaningful variable.
+	//
+	client.on('torrent', function (torrent) {
+		torrent.resume();
+	});
+
+	let magnet = req.params.magnet;
+
+	//
+	//	2.	Returns the torrent with the given torrentId. Convenience method.
+	//		Easier than searching through the client.torrents array. Returns
+	//		null if no matching torrent found.
+	//
+	var tor = client.get(magnet);
+
+	//
+	//	3.	Variable that will store the user selected file
+	//
+	let file = {};
+
+	//
+	//	4.	Loop over all the files contained inside a Magnet Hash and find the one
+	//		the user selected.
+	//
+	console.log("Go \O/")
+	for (i = 0; i < tor.files.length; i++) {
+		if (tor.files[i].name == decodeURI(req.params.file_name)) {
+			file = tor.files[i];
+		}
+	}
+
+	//
+	//	5.	Save the range the browser is asking for in a clear and
+	//		reusable variable
+	//
+	//		The range tells us what part of the file the browser wants
+	//		in bytes.
+	//
+	//		EXAMPLE: bytes=65534-33357823
+	//
+	let range = req.headers.range;
+
+	console.log(range);
+
+	//
+	//	6.	Make sure the browser ask for a range to be sent.
+	//
+	if (!range) {
+		//
+		// 	1.	Create the error
+		//
+		let err = new Error("Wrong range");
+		err.status = 416;
+
+		//
+		//	->	Send the error and stop the request.
+		//
+		return next(err);
+	}
+
+	//
+	//	7.	Convert the string range in to an array for easy use.
+	//
+	let positions = range.replace(/bytes=/, "").split("-");
+
+	//
+	//	8.	Convert the start value in to an integer
+	//
+	let start = parseInt(positions[0], 10);
+
+	//
+	//	9.	Save the total file size in to a clear variable
+	//
+	let file_size = file.length;
+
+	//
+	//	10.	IF 		the end parameter is present we convert it in to an
+	//				integer, the same way we did the start position
+	//
+	//		ELSE 	We use the file_size variable as the last part to be
+	//				sent.
+	//
+	let end = positions[1] ? parseInt(positions[1], 10) : file_size - 1;
+
+	//
+	//	11.	Calculate the amount of bits will be sent back to the
+	//		browser.
+	//
+	let chunksize = (end - start) + 1;
+
+	//
+	//	12.	Create the header for the video tag so it knows what is
+	//		receiving.
+	//
+	let head = {
+		"Content-Range": "bytes " + start + "-" + end + "/" + file_size,
+		"Accept-Ranges": "bytes",
+		"Content-Length": chunksize,
+		"Content-Type": "video/mp4"
+	}
+
+	//
+	//	13.	Send the custom header
+	//
+	res.writeHead(206, head);
+
+	//
+	//	14.	Create the createReadStream option object so createReadStream
+	//		knows how much data it should be read from the file.
+	//
+	let stream_position = {
+		start: start,
+		end: end
+	}
+
+	//
+	//	15.	Create a stream chunk based on what the browser asked us for
+	//
+	let stream = file.createReadStream(stream_position)
+
+	//
+	//	16.	Pipe the video chunk to the request back to the request
+	//
+	stream.pipe(res);
+
+	//
+	//	->	If there was an error while opening a stream we stop the
+	//		request and display it.
+	//
+	stream.on("error", function (err) {
+
+		return next(err);
+
+	});
+
+});
+
+//
+//	The API call that gets all the Magnet Hashes that the client is actually
+//	having.
+//
+//	return 		<-	An array with all the Magnet Hashes
+//
+router.get('/list2', function (req, res, next) {
+
+
+	res.status(200);
+	res.json({ movies: files });
+
+});
+
+router.post('/upload', (req, res) => {
+	new formidable.IncomingForm().parse(req)
+		.on('field', (name, field) => {
+			console.log('Field', name, field)
+		})
+		.on('file', (name, file) => {
+			console.log('Uploaded file', file.path + '/' + file.name)
+			var data = fs.readFileSync(file.path);
+			client.add(data, function (torrent) {
+
+				//
+				//	->	Once we have all the data send it back to the browser to be
+				//		displayed.
+				//
+
+				getInfo(torrent, torrent.infoHash, req.get('host')).then(() => {
+					torrent.pause();
+					// res.status(200);
+					// res.json({ movies: files });
+					console.log('All done!');
+				});
+			});
+		})
+		.on('aborted', () => {
+			console.error('Request aborted by the user')
+		})
+		.on('error', (err) => {
+			console.error('Error', err)
+			throw err
+		})
+		.on('end', () => {
+			res.redirect('/');
+		})
+
+})
+
+router.get('/list', function (req, res, next) {
+
+	//
+	//	1.	Loop over all the Magnet Hashes
+	//                           .reduce
+	//let torrent = client.torrents.forEach(function(array, data) {
+	let array = []
+	client.torrents.forEach(function (data) {
+
+		array.push({
+			name: data.name,
+			hash: data.infoHash,
+			size: data.length
+		});
+		return array;
+
+	}); //[]);
+
+
+	//
+	//	->	Return the Magnet Hashes
+	//
+	res.status(200);
+	res.json(array);
+
+});
+
+//
+//	The API call that sends back the stats of the client
+//
+//	return 		<-	A object with the client stats
+//
+router.get('/stats', function (req, res, next) {
+
+	res.status(200);
+	res.json(stats);
+
+});
+
+//
+//	The API call that gets errors that occurred with the client
+//
+//	return 		<-	A a string with the error
+//
+router.get('/errors', function (req, res, next) {
+
+	res.status(200);
+	res.json(error_message);
+
+});
+
+//
+//	The API call to delete a Magnet Hash from the client.
+//
+//	magnet 		-> 	Magnet Hash
+//
+//	return 		<-	Just the status of the request
+//
+router.get('/delete/:hash', function (req, res, next) {
+
+	//
+	//	1.	Extract the magnet Hash and save it in a meaningful variable.
+	//
+	let torrent = client.get(req.params.hash);
+	let torrentPath = torrent.path;
+	//
+	//	2.	Remove the Magnet Hash from the client.
+	//
+	client.remove(torrent, function (torrent) {
+
+		//
+		//  3. Update files list
+		//
+
+		var removeIndex = files.map(function (item) { return item.hash; }).indexOf(torrent);
+		files.splice(removeIndex, 1);
+		rimraf(torrentPath, { maxBusyTries: 10 }, e => {
+			if (e) {
+				console.error(e);
+			} else {
+				console.log("Deleted torrent:", torrentPath);
+			}
+		});
+		res.status(200);
+		res.json({ movies: files });
+	});
+});
+
+module.exports = router;
+
